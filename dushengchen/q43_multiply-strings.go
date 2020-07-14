fce52b72d07129d600c8f1dfa62f1d85dc809f7b
helloworld-du <dushengch100@126.com>
2020-07-14 22:10:40 +0800

    q43

--- dushengchen/q43_multiply-strings.go
+++ dushengchen/q43_multiply-strings.go
@@ -1,36 +1,57 @@
 package dushengchen
 
-import "fmt"
 
 /**
 Submission:
     https://leetcode.com/submissions/detail/366393972/
 */
-func firstMissingPositive(nums []int) int {
-    for i, v := range  nums {
-        if v <= 0 {
-            nums[i] = -2
+func multiply(num1 string, num2 string) string {
+    a := []rune(num1)
+    b := []rune(num2)
+    sum := make([]rune, len(num1)+len(num2))
+    sum[0] = '0'
+    for i := 0; i < len(a); i++ {
+        m := runeToInt(a[len(a)-i-1])
+        for j := 0; j < len(b); j++ {
+            n := runeToInt(b[len(b)-j-1])
+            x :=  m * n
+            for add := 0;; add++{
+                c := runeToInt(sum[i+j+add])
+                if c > 9 || c < 0 {
+                    c = 0
+                }
+                x = x+c
+                rest := x % 10
+                sum[i+j+add] = intToRune(rest)
+                x = x / 10
+                // fmt.Println(m, n, c, x, string(sum))
+                if  x == 0 {
+                    break
+                }
+            }
+        }
+        // fmt.Println(i, m, string(sum))
+    }
+    ret := []rune{}
+    allZero := true
+    for i := cap(sum)-1; i >=0; i-- {
+        if m := runeToInt(sum[i]); m >= 0 && m <= 9 {
+            ret = append(ret, sum[i])
+            if m != 0 {
+                allZero = false
+            }
         }
     }
-    for _, v := range  nums {
-        set(nums, v)
+    if allZero {
+        return "0"
     }
-    fmt.Println(nums)
-    for i, v := range  nums {
-        if v != -1 {
-            return i+1
-        }
-    }
-    return len(nums)+1
+    return string(ret)
 }
 
-func set(nums []int, target int) {
-    if target > len(nums) || target <= 0 {
-        return
-    }
-    x := nums[target-1]
-    nums[target-1] = -1
-    if x != target {
-        set(nums, x)
-    }
+func runeToInt(c rune) int {
+    return int(c - '0')
+}
+
+func intToRune(i int) rune {
+    return rune(i) + '0'
 }
