64ae8db1fc3167830e056a2443780cf86e474ca5
helloworld-du <dushengch100@126.com>
2020-11-16 14:52:48 +0800

    add q72

---
+++ dushengchen/q72_edit-distance.go
@@ -0,0 +1,12 @@
+package dushengchen
+
+/**
+Submission:
+	https://leetcode.com/submissions/detail/375851921/
+
+Runtime: 0 ms, faster than 100.00% of Go online submissions for Plus One.
+Memory Usage: 2 MB, less than 93.94% of Go online submissions for Plus One.
+*/
+func minDistance(word1 string, word2 string) int {
+    return 0
+}
