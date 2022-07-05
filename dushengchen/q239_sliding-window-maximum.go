f6816895186936374182e64726c0997bebb60f36
dushengchen <dushengchen.dsc@alibaba-inc.com>
2022-07-05 20:10:12 +0800

    临时代码

---
+++ dushengchen/q239_sliding-window-maximum.go
@@ -0,0 +1,9 @@
+package dushengchen
+
+//func maxSlidingWindow(nums []int, k int) []int {
+//	h := (IntHeap)(nums[0:k])
+//	heap.Init(h)
+//	for i:= k+1;i<len(nums); i++ {
+//		heap.Remove()
+//	}
+//}
